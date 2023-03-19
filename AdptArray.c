4116d1eb3c258c27df2cc34b1a3e1499bafed041
Malak47 <malak.qeedan@hotmail.com>
2023-03-19 20:12:20 +0200

    Uploading Code Files

---
+++ AdptArray.c
@@ -0,0 +1,33 @@
+//
+// Created by malak on 3/17/23.
+//
+
+#include "AdptArray.h"
+#include "book.h"
+#include "Person.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+PAdptArray CreateAdptArray(COPY_FUNC, DEL_FUNC, PRINT_FUNC) {
+
+}
+
+void DeleteAdptArray(PAdptArray) {
+
+}
+
+Result SetAdptArrayAt(PAdptArray, int, PElement) {
+
+}
+
+PElement GetAdptArrayAt(PAdptArray, int) {
+}
+
+int GetAdptArraySize(PAdptArray) {
+
+}
+
+void PrintDB(PAdptArray) {
+
+}
