e62afec3b9b6cf736279f5519884b326a1586f74
Malak47 <malak.qeedan@hotmail.com>
2023-03-20 22:55:44 +0200

    Implementing AdptArray

--- AdptArray.c
+++ AdptArray.c
@@ -2,32 +2,94 @@
 // Created by malak on 3/17/23.
 //
 
-#include "AdptArray.h"
-#include "book.h"
-#include "Person.h"
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+#include "AdptArray.h"
 
-PAdptArray CreateAdptArray(COPY_FUNC, DEL_FUNC, PRINT_FUNC) {
+typedef struct AdptArray_ {
+    PElement *pElem;
+    int size;
+    COPY_FUNC copyFunc;
+    DEL_FUNC delFunc;
+    PRINT_FUNC printFunc;
+} AdptArray;
+
+PAdptArray CreateAdptArray(COPY_FUNC copyFunc, DEL_FUNC delFunc, PRINT_FUNC printFunc) {
+    PAdptArray pAdp = (PAdptArray) malloc(sizeof(AdptArray));
+    if (pAdp) {
+        pAdp->pElem = NULL;
+        pAdp->size = 0;
+        pAdp->copyFunc = copyFunc;
+        pAdp->delFunc = delFunc;
+        pAdp->printFunc = printFunc;
+        return pAdp;
+    }
+    return NULL;
 
 }
 
-void DeleteAdptArray(PAdptArray) {
-
+void DeleteAdptArray(PAdptArray pAdp) {
+    int i = 0;
+    while (i < pAdp->size) {
+        if (pAdp->pElem[i]) {
+            pAdp->delFunc(pAdp->pElem[i]);
+        }
+        i++;
+    }
+    free(pAdp->pElem);
+    free(pAdp);
 }
 
-Result SetAdptArrayAt(PAdptArray, int, PElement) {
+Result SetAdptArrayAt(PAdptArray pAdp, int index, PElement newpElem) {
+    if (!pAdp || index < 0 || !newpElem) {
+        return FAIL;
+    }
 
+
+    if (index >= pAdp->size) {
+        PElement newPElem = (PElement *) calloc(index + 1, sizeof(PElement));
+        if (!newPElem) {
+            return FAIL;
+        }
+        memcpy(newPElem, pAdp->pElem, pAdp->size * sizeof(newpElem));
+        free(pAdp->pElem);
+        pAdp->pElem = newPElem;
+    }
+    //CHECK
+    if (pAdp->pElem[index]) {
+        pAdp->delFunc(pAdp->pElem[index]);
+    }
+
+    pAdp->pElem[index] = pAdp->copyFunc(newpElem);
+    if (pAdp->size <= index) {
+        pAdp->size = index + 1;
+    }
+    return SUCCESS;
 }
 
-PElement GetAdptArrayAt(PAdptArray, int) {
+PElement GetAdptArrayAt(PAdptArray pAdp, int index) {
+    if (!pAdp || index < 0 || pAdp->size < index) {
+        return NULL;
+    }
+    if (pAdp->pElem[index]) {
+        return pAdp->copyFunc(pAdp->pElem[index]);
+    }
+    return NULL;
 }
 
-int GetAdptArraySize(PAdptArray) {
-
+int GetAdptArraySize(PAdptArray pAdp) {
+    if (!pAdp || pAdp->size < 0) {
+        return -1;
+    }
+    return pAdp->size;
 }
 
-void PrintDB(PAdptArray) {
-
+void PrintDB(PAdptArray pAdp) {
+    int i = 0;
+    while (i < pAdp->size) {
+        if (pAdp->pElem[i]) {
+            pAdp->printFunc(pAdp->pElem[i]);
+        }
+        i++;
+    }
 }
